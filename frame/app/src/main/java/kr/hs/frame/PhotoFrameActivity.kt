ccdcc9f32e02393a8f7e301dcc463fb5fa1c8c02
bhindor <82446373+bhindor@users.noreply.github.com>
2022-07-28 21:35:30 +0900

    frame

--- frame/app/src/main/java/kr/hs/frame/PhotoFrameActivity.kt
+++ frame/app/src/main/java/kr/hs/frame/PhotoFrameActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import java.util.*
 import kotlin.concurrent.timer
 
 class PhotoFrameActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class PhotoFrameActivity : AppCompatActivity() {
     private val photoList = mutableListOf<Uri>()
 
     private var currentPosition = 0
+
+    private var timer: Timer? = null
 
     private val photoImageView: ImageView by lazy {
         findViewById(R.id.photoImageView)
@@ -24,8 +27,6 @@ class PhotoFrameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_photo_frame)
 
         getPhotoUriFromIntent()
-
-        startTimer()
     }
 
     private fun getPhotoUriFromIntent() {
@@ -38,7 +39,7 @@ class PhotoFrameActivity : AppCompatActivity() {
     }
 
     private fun startTimer() {
-        kotlin.concurrent.timer(period = 5 * 1000) {
+        timer = kotlin.concurrent.timer(period = 5 * 1000) {
             runOnUiThread {
                 val current = currentPosition
                 val next = if (photoList.size <= currentPosition + 1) 0 else currentPosition + 1
@@ -54,5 +55,23 @@ class PhotoFrameActivity : AppCompatActivity() {
                 currentPosition = next
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        timer?.cancel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        
+        startTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        timer?.cancel()
     }
 }
