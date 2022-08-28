6c46ebd29692df4e33d1a9816bbfebaeffb55ea1
bhindor <82446373+bhindor@users.noreply.github.com>
2022-08-29 08:05:36 +0900

    recorder
    2022-08-29

---
+++ recorder/app/src/main/java/kr/hs/recorder/MainActivity.kt
@@ -0,0 +1,24 @@
+package kr.hs.recorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val recordButton: RecordButton by lazy {
+        findViewById(R.id.recordButton)
+    }
+
+    private val state = State.BEFORE_RECORDING
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        recordButton.updateIconWithState(state)
+    }
+}
