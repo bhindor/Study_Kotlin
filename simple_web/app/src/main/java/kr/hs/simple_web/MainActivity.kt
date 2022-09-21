8e9d071e0703456341d4da8fd893e4ef3f5b8dd3
bhindor <82446373+bhindor@users.noreply.github.com>
2022-09-21 17:17:58 +0900

    webview
    기능 개발

--- simple_web/app/src/main/java/kr/hs/simple_web/MainActivity.kt
+++ simple_web/app/src/main/java/kr/hs/simple_web/MainActivity.kt
@@ -1,11 +1,86 @@
 package kr.hs.simple_web
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val goHomeButton: ImageButton by lazy {
+        findViewById(R.id.goHomeButton)
+    }
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
+    private val goBackButton: ImageButton by lazy {
+        findViewById(R.id.goBackButton)
+    }
+
+    private val goForwardButton: ImageButton by lazy {
+        findViewById(R.id.goForwardButton)
+    }
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
+    }
+    
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl(DEFAULT_URL)
+        }
+    }
+
+    private fun bindViews() {
+        goHomeButton.setOnClickListener {
+            //webView.loadUrl("http://www.google.come")
+            webView.loadUrl(DEFAULT_URL)
+        }
+
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
+        goBackButton.setOnClickListener {
+            webView.goBack()
+        }
+        goForwardButton.setOnClickListener {
+            webView.goForward()
+        }
+        goHomeButton.setOnClickListener {
+
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_URL = "http://www.google.com"
     }
 }
