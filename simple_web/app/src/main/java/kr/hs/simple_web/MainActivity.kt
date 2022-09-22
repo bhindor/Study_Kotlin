2b5faa0284255b530c997b252b7d5f25bf3cc11b
bhindor <82446373+bhindor@users.noreply.github.com>
2022-09-22 14:23:34 +0900

    siimple_web
    2022-09-22

--- simple_web/app/src/main/java/kr/hs/simple_web/MainActivity.kt
+++ simple_web/app/src/main/java/kr/hs/simple_web/MainActivity.kt
@@ -1,13 +1,18 @@
 package kr.hs.simple_web
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,8 +32,16 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.goForwardButton)
     }
 
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById(R.id.refreshLayout)
+    }
+
     private val webView: WebView by lazy {
         findViewById(R.id.webView)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         initViews()
         bindViews()
     }
-    
+
     override fun onBackPressed() {
         if(webView.canGoBack()) {
             webView.goBack()
@@ -51,6 +64,7 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         webView.apply {
             webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
             loadUrl(DEFAULT_URL)
         }
@@ -64,7 +78,12 @@ class MainActivity : AppCompatActivity() {
 
         addressBar.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl(v.text.toString())
+                val loadingUrl = v.text.toString()
+                if(URLUtil.isNetworkUrl(loadingUrl)){
+                    webView.loadUrl(loadingUrl)
+                } else {
+                    webView.loadUrl("http://$loadingUrl")
+                }
             }
 
             return@setOnEditorActionListener false
@@ -75,8 +94,35 @@ class MainActivity : AppCompatActivity() {
         goForwardButton.setOnClickListener {
             webView.goForward()
         }
-        goHomeButton.setOnClickListener {
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
 
+    inner class WebViewClient: android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+            goBackButton.isEnabled = webView.canGoBack()
+            goForwardButton.isEnabled = webView.canGoForward()
+            addressBar.setText(url)
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
         }
     }
 
