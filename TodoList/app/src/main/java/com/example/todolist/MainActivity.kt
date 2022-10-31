90c1bb4889292255abbb21871e63b57b6acc7806
bhindor <bhindor@dgsw.hs.kr>
2022-11-01 08:15:53 +0900

    modify:MainActivity add:WriteFragment

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.todolist.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,21 +17,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction().add(binding.bodyLayout.id, HomeFragment()).commit()
-
-        binding.bottomMenu.setOnNavigationItemReselectedListener { item ->
-            when(item.itemId){
-                R.id.homeFragment -> {
-                    replaceFragment(HomeFragment())
-                    true
-                }
-                R.id.registerFragment -> {
-                    //replaceFragment()
-                }
+        val bottomNavigationView = findViewById<View>(R.id.bottom_menu) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().replace(binding.bodyLayout.id, HomeFragment()).commit()
+    }
+//    private fun replaceFragment(fragment: Fragment) {
+//        supportFragmentManager.beginTransaction().add(binding.bodyLayout.id, fragment).commit()
+//    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.home -> {
+                supportFragmentManager.beginTransaction().replace(binding.bodyLayout.id, HomeFragment()).commit()
+            }
+            R.id.write -> {
+                supportFragmentManager.beginTransaction().replace(binding.bodyLayout.id, WriteFragment()).commit()
             }
         }
-    }
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().add(binding.bodyLayout.id, fragment).commit()
+        return true
     }
 }
---
+++ TodoList/app/src/main/java/com/example/todolist/WriteFragment.kt
@@ -0,0 +1,28 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todolist.databinding.FragmentWriteBinding
+
+class WriteFragment : Fragment() {
+
+    private lateinit var binding: FragmentWriteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
