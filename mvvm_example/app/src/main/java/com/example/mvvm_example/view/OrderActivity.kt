b6a7494f9c92f607f07826dcef00b28147714564
bhindor <bhindor@naver.com>
2022-11-23 15:56:40 +0900

    mvvm_example

---
+++ mvvm_example/app/src/main/java/com/example/mvvm_example/view/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvm_example.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvm_example.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
